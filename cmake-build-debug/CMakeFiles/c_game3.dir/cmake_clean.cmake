54488267f3a5fc5058a0a66776d148a270305e43
ethaiyi9 <yiceyuan@126.com>
2021-12-31 16:56:40 +0800

    坦克大战完成版

---
+++ cmake-build-debug/CMakeFiles/c_game3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_game3.dir/main.cpp.obj"
+  "c_game3.exe"
+  "c_game3.exe.manifest"
+  "c_game3.lib"
+  "c_game3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_game3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
